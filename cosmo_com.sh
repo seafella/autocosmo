e0d47352f58d7429e60bb1f8797b0d48bea63012
steev <s@te.ev>
2024-01-26 17:57:07 -0600

    build an APE from a .py file .. py2APE

---
+++ cosmo_com.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+set -eux
+
+# The first command line argument is the filename
+FILENAME=$1
+
+PREV="$(pwd)"
+# DIR="$(mktemp -d)"
+DIR=./temp
+cp "$FILENAME" "$DIR"
+
+# Delete .args file if it exists and create a new one with specified content
+if [ -f .args ]; then
+  rm .args
+fi
+echo "$FILENAME" > .args
+echo "..." >> .args
+
+cp .args "$DIR"
+# cp repl.html "$DIR"
+# cp style.css "$DIR"
+cd "$DIR"
+wget https://cosmo.zip/pub/cosmos/bin/python
+wget https://cosmo.zip/pub/cosmos/bin/zip
+chmod +x python
+chmod +x zip
+./python -m compileall "$FILENAME"
+mkdir Lib
+
+# Extract the filename without the extension for use in the compiled filename
+BASENAME=$(basename "$FILENAME" .py)
+cp "__pycache__/${BASENAME}."*.pyc "Lib/${BASENAME}.pyc"
+# cp style.css repl.html Lib
+cp python "${BASENAME}.com"
+./zip -r "${BASENAME}.com" Lib .args
+echo "Testing..."
+./"${BASENAME}.com"
+cd "$PREV"
+cp "$DIR/${BASENAME}.com" .
